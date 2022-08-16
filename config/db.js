1011fc08773c8651d9753d872756091aedb11994
masudRana88 <mahinuddin88@gmail.com>
2022-08-16 14:08:35 +0600

    Connect to db

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDB = async()=>{
+    try{
+        const conn= await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgxio.mongodb.net/test`)
+        console.log(`MongoDB Connected ${conn.connection.host}`)
+    }
+    catch(error){
+        console.log(`Error : ${error.message}`)
+        process.exit(1)
+    }
+}
+
+
+exports.connectToDB = connectToDB
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const {connectToDB} = require("./config/db")
+require('dotenv').config()
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+connectToDB()
 
 app.get("/", (req,res)=>{
     res.send("server is running");
